2903077a23945c63383fcfc367eb4ca3b059e8d3
Zeeenx <87702906+Zeeenx@users.noreply.github.com>
2021-09-15 12:56:21 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -10,14 +10,14 @@ from helper_func import encode, get_message_id
 async def batch(client: Client, message: Message):
     while True:
         try:
-            first_message = await client.ask(text = "Terusan pesan pertama dari databese ⚡️ \nhttps://t.me/joinchat/8_mLkvhnvcBmOWY1", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            first_message = await client.ask(text = "Terusan pesan pertama dari databese ⚡️ \n\nhttps://t.me/joinchat/8_mLkvhnvcBmOWY1", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         f_msg_id = await get_message_id(client, first_message)
         if f_msg_id:
             break
         else:
-            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            await first_message.reply("❌", quote = True)
             continue
 
     while True:
@@ -29,7 +29,7 @@ async def batch(client: Client, message: Message):
         if s_msg_id:
             break
         else:
-            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            await second_message.reply("❌", quote = True)
             continue
 
 
@@ -37,7 +37,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ ʙᴀɢɪᴋᴀɴ ʟɪɴᴋ", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>ʟɪɴᴋ ʙᴇʀʜᴀsɪʟ ᴅɪʙᴜᴀᴛ ✅</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"<b>link :</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -57,4 +57,4 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ ʙᴀɢɪᴋᴀɴ ʟɪɴᴋ", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>ʟɪɴᴋ ʙᴇʀʜᴀsɪʟ ᴅɪʙᴜᴀᴛ ✅</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>link :</b>\n\n{link}", quote=True, reply_markup=reply_markup)
